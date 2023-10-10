168e15f1d7d6e1a44be0d235f670501348582224
zaytsev_dv <d.zaytsev@pulsiteam.ru>
2023-10-10 20:26:16 +0300

    refactoring part 1

--- src/main/java/com/awesome/cli/commands/CloudSettingCommand.java
+++ src/main/java/com/awesome/cli/commands/CloudSettingCommand.java
@@ -4,9 +4,6 @@ import com.awesome.cli.application.util.CommandTab;
 import com.awesome.cli.application.util.PromptColor;
 import com.awesome.cli.application.util.ShellHelper;
 import lombok.AllArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.shell.command.CommandRegistration;
-import org.springframework.shell.context.InteractionMode;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
@@ -14,14 +11,13 @@ import org.springframework.shell.standard.ShellOption;
 @ShellComponent
 @AllArgsConstructor
 public class CloudSettingCommand {
-    private final ShellHelper shellHelper;
 
     @ShellMethod(value = "create bucket in cloud", key = "create-bucket")
     public String createBucket(
             @ShellOption(value = {"--n"}, help = "name for your bucket", valueProvider = CommandTab.class) String bucketName,
             @ShellOption(value = {"--p"}, help = "is public bucket", defaultValue = "false", valueProvider = CommandTab.class) String isPublic
     ) {
-        return this.shellHelper.getColored("Dont impl yet", PromptColor.RED);
+        return ShellHelper.getColored("Dont impl yet", PromptColor.RED);
     }
 
     @ShellMethod(value = "setup cloud settings", key = "setup-cloud-settings")
@@ -30,6 +26,6 @@ public class CloudSettingCommand {
             @ShellOption(value = {"--sk"}, help = "secret-key for cloud account", valueProvider = CommandTab.class) String secretKey,
             @ShellOption(value = {"--h"}, help = "host for cloud account", valueProvider = CommandTab.class) String host
     ) {
-        return this.shellHelper.getColored("Dont impl yet", PromptColor.RED);
+        return ShellHelper.getColored("Dont impl yet", PromptColor.RED);
     }
 }
